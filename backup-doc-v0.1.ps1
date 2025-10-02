e5e7f40aea4fb5e7fb57052deda11f31a1a357b8
josue197301 <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>
2025-10-02 11:29:20 -0300

    Ajuste de nomenclatura de módulos

--- backup-doc-v0.1.ps1
+++ backup-doc-v0.1.ps1
@@ -1,4 +1,6 @@
-﻿Import-Module  ".\lib\SMBFileTools.psm1"
+﻿Import-Module  ".\lib\PNFilesTools.psm1"
+Import-Module  ".\lib\PNDataTools.psm1"
+
 # Carrega JSON de configuracoes
 $configPath = ".\lib\backup-config-v0.1.json"
 $config = Get-Content $configPath | ConvertFrom-Json
@@ -34,9 +36,25 @@ Get-ChildItem $args[0] -Directory | ForEach-Object {
     #Write-Output "$($_.FullName)"
 
     Get-ChildItem "$($_.FullName)" -File -Recurse | ForEach-Object {
-        #Write-Output "---- $($_.FullName)"
         $fp = Get-FileProperties -FilePath  "$($_.FullName)"
-        $fp | Format-Table
+        $fp.baseDir + "\" +$fp.baseName
+        # formar query de busca
+        $queryBuscaArquivo = "select max(arq_id), arq_nome, arq_path, date_trunc('minute',arq_ts_mod) as arq_ts_modif from arquivo WHERE date_trunc('minute', arq_ts_mod) = '$($fp.tsMod_F)'::timestamp AND arq_nome='$($fp.baseName)' AND arq_tam='$($fp.size)' AND arq_tipo='$($fp.extension)' group by arq_nome, arq_path, arq_ts_mod;";
+        $queryBuscaArquivo ;
+        # buscar correspondencia no banco
+        $resBusca = Invoke-PostgresQuery -PgHost "$($config.database.host)" -Database "$($config.database.dbname)" -User "$($config.database.username)" -Query "$($queryBuscaArquivo)" -ReturnAsCsvObject;
+        Write-Host "------------------`n# Ts - "$fp.baseName": "$fp.tsMod_F" | "$fp.tsMod
+        if ( $resBusca.Count  -gt 0 ){ # encontrou correspondencia
+            Write-Host "### Encontrado no banco: "$resBusca.arq_nome" | "$resBusca.arq_path" | "$resBusca.arq_ts_modif;
+            $resBusca.arq_ts_modif
+        } else { # registrar no banco e zipar
+            
+            # formar o nome do arquivo para zipar
+            # - tirar md5 ? <não>
 
+            <#$zipFileName = "$($fp.baseDir)\$($fp.baseName)_$($resBusca.arq_id)_$($fp.tsMod_F).zip"
+            Write-Host "### Zipando para: "$zipFileName
+            Compress-Archive -Path "$($fp.fullName)" -DestinationPath "$zipFileName" -Force #>
+        }
     }
 }
